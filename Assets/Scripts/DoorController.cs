de20592add33c8ff5f5f7da8abc1e62c53d151b2
ablb9 <ablb9@aim.omaha.lib.ne.us>
2018-07-07 10:46:16 -0500

    save buttons

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour {
+    public Button Button1;
+    // Use this for initialization
+    void Start () {
+        Button btn1 = Button1.GetComponent<Button>();
+        btn1.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+
+}
