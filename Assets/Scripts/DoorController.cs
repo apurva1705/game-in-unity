3d8184ee4e3f58c7ac39735318eca7118d46bd87
ablb8 <ablb8@aim.omaha.lib.ne.us>
2018-07-28 12:39:51 -0500

    particle systems

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -7,17 +7,29 @@ public class DoorController : MonoBehaviour {
     public Button Button1;
     public GameObject Room2;
     public GameObject Parent;
+    public GameObject particlePrefab;
+
     // Use this for initialization
-    void Start () {
+    void Start() {
         Button btn1 = Button1.GetComponent<Button>();
         btn1.onClick.AddListener(TaskOnClick);
     }
+
     void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
-        Vector3 pos = new Vector3(0,0,0);
+        StartCoroutine("CreateParticleSystem");
+        Vector3 pos = new Vector3(0, 0, 0);
         Instantiate(Room2, pos, Quaternion.identity);
         Destroy(Parent);
+    }
+
+    IEnumerator CreateParticleSystem()
+    {
+
+        GameObject particles = (GameObject)Instantiate(particlePrefab);
+
+        yield return new WaitForSeconds(2.5f);
     }
 
 
